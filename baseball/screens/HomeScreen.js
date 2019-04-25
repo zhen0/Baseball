87b8b9b9bdbf37f4bf0084e056817fcb953c40ed
Jenny Grange <jenniferhelengrange@gmail.com>
2019-04-25 10:48:06 -0400

    adds camera roll and basic homescreen

---
+++ baseball/screens/HomeScreen.js
@@ -0,0 +1,226 @@
+import React from "react";
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
+import { WebBrowser } from "expo";
+
+import { MonoText } from "../components/StyledText";
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
+          <View style={styles.welcomeContainer}>
+            <Image
+              source={require("../assets/images/baseball.png")}
+              style={styles.welcomeImage}
+            />
+          </View>
+
+          <View style={styles.getStartedContainer}>
+            <Text style={styles.getStartedText}>
+              Welcome to Baseball Bandersnatch!
+            </Text>
+
+            <View
+              style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
+            >
+              <MonoText style={styles.codeHighlightText}>
+                Choose a Player to get started
+              </MonoText>
+            </View>
+
+            <Text style={styles.getStartedText}>
+              You will be deciding how they play today!
+            </Text>
+          </View>
+
+          <View style={styles.helpContainer}>
+            <TouchableOpacity onPress={this._handlePlayer1}>
+              <Image
+                source={{
+                  uri:
+                    "https://secure.i.telegraph.co.uk/multimedia/archive/02636/arod_2636286b.jpg"
+                }}
+                style={styles.playerImage}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this._handlePlayer2}>
+              <Image
+                source={{
+                  uri:
+                    "https://hips.hearstapps.com/hbz.h-cdn.co/assets/cm/15/04/54bd3d512cfd2_-_hbz-mlb-david-wright-487011951.jpg?crop=1.0xw:1xh;center,top&resize=980:*"
+                }}
+                style={styles.playerImage}
+              />
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+
+        <View style={styles.tabBarInfoContainer}>
+          <Text style={styles.tabBarInfoText}>
+            This is a tab bar. You can edit it in:
+          </Text>
+
+          <View
+            style={[styles.codeHighlightContainer, styles.navigationFilename]}
+          >
+            <MonoText style={styles.codeHighlightText}>
+              navigation/MainTabNavigator.js
+            </MonoText>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  // _maybeRenderDevelopmentModeWarning() {
+  //   if (__DEV__) {
+  //     const learnMoreButton = (
+  //       <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
+  //         Learn more
+  //       </Text>
+  //     );
+
+  //     return (
+  //       <Text style={styles.developmentModeText}>
+  //         Development mode is enabled, your app will be slower but you can use
+  //         useful development tools. {learnMoreButton}
+  //       </Text>
+  //     );
+  //   } else {
+  //     return (
+  //       <Text style={styles.developmentModeText}>
+  //         You are not in development mode, your app will run at full speed.
+  //       </Text>
+  //     );
+  //   }
+  // }
+
+  _handleLearnMorePress = () => {
+    WebBrowser.openBrowserAsync(
+      "https://docs.expo.io/versions/latest/guides/development-mode"
+    );
+  };
+
+  _handlePlayer1 = () => {
+    WebBrowser.openBrowserAsync(
+      "https://www.mlb.com/video/marcus-semien-homers-4-on-a-line-drive-to-left-field-jurickson-profar-scores-nic?t=most-popular"
+    );
+  };
+  _handlePlayer2 = () => {
+    WebBrowser.openBrowserAsync(
+      "https://www.mlb.com/video/statcast-cain-s-running-catch?t=most-popular"
+    );
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: "rgba(0,0,0,0.4)",
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: "center"
+  },
+  contentContainer: {
+    paddingTop: 30
+  },
+  welcomeContainer: {
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 20
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: "contain",
+    marginTop: 3,
+    marginLeft: -10
+  },
+  playerImage: {
+    width: 200,
+    height: 160,
+    resizeMode: "contain",
+    marginTop: 3,
+    marginLeft: -10
+  },
+  getStartedContainer: {
+    alignItems: "center",
+    marginHorizontal: 50
+  },
+  homeScreenFilename: {
+    marginVertical: 7
+  },
+  codeHighlightText: {
+    color: "rgba(96,100,109, 0.8)"
+  },
+  codeHighlightContainer: {
+    backgroundColor: "rgba(0,0,0,0.05)",
+    borderRadius: 3,
+    paddingHorizontal: 4
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: "rgba(96,100,109, 1)",
+    lineHeight: 24,
+    textAlign: "center"
+  },
+  tabBarInfoContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: "black",
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3
+      },
+      android: {
+        elevation: 20
+      }
+    }),
+    alignItems: "center",
+    backgroundColor: "#fbfbfb",
+    paddingVertical: 20
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: "rgba(96,100,109, 1)",
+    textAlign: "center"
+  },
+  navigationFilename: {
+    marginTop: 5
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: "center"
+  },
+  helpLink: {
+    paddingVertical: 15
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: "#2e78b7"
+  }
+});
---
+++ baseball/screens/SettingsScreen.js
@@ -0,0 +1,91 @@
+import React from "react";
+import {
+  Image,
+  Button,
+  ScrollView,
+  View,
+  CameraRoll,
+  StyleSheet
+} from "react-native";
+import { Permissions } from "expo";
+
+export default class SettingsScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      photos: []
+    };
+  }
+  _handleButtonPress = () => {
+    this._loadImageClick();
+    CameraRoll.getPhotos({
+      first: 20,
+      assetType: "Photos"
+    })
+      .then(r => {
+        this.setState({ photos: r.edges });
+      })
+      .catch(err => {
+        console.log(err); //Error Loading Images
+      });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Load Images"
+          onPress={this._handleButtonPress}
+          style={styles.button}
+        />
+        <ScrollView>
+          {this.state.photos.map((p, i) => {
+            return (
+              <Image
+                key={i}
+                style={styles.playerImage}
+                source={{ uri: p.node.image.uri }}
+              />
+            );
+          })}
+        </ScrollView>
+      </View>
+    );
+  }
+
+  _loadImageClick = async () => {
+    try {
+      // permissions returns only for location permissions on iOS and under certain conditions, see Permissions.LOCATION
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== "granted") {
+        throw new Error("Camera permission not granted");
+      }
+    } catch (err) {
+      console.log("error in camera roll permissions", err);
+    }
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 60,
+    alignItems: "center"
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: "center",
+    backgroundColor: "#4842f4"
+  },
+  buttonText: {
+    padding: 20,
+    color: "green"
+  },
+  playerImage: {
+    width: 200,
+    height: 160,
+    resizeMode: "contain",
+    marginTop: 3,
+    marginLeft: -10
+  }
+});
