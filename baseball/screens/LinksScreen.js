6174822a4799b3de8b09dd6994ed61ed7d71094c
Jenny G <40272060+zhen0@users.noreply.github.com>
2019-04-27 13:28:39 -0400

    Update LinksScreen.js

--- baseball/screens/LinksScreen.js
+++ baseball/screens/LinksScreen.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Help',
   };
 
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        
+           
+        <Text>For extra help you can see the video here: </Text>
       </ScrollView>
     );
   }
