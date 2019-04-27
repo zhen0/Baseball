55ac53b0e5bc908bb8d62165e6c01bd3579023b1
Jenny <jenniferhelengrange@gmail.com>
2019-04-27 18:28:49 -0400

    bug gone

--- baseball/screens/Lesson.js
+++ baseball/screens/Lesson.js
@@ -33,7 +33,7 @@ export default class HomeScreen extends React.Component {
 
   render() {
     return (
-      <View style={StyleSheet.container}>
+      <ScrollView style={StyleSheet.container}>
         <View style={styles.welcomeContainer}>
           <Text style={styles.getStartedText}> ---------- </Text>
           <Image
@@ -47,43 +47,45 @@ export default class HomeScreen extends React.Component {
         </View>
         <View style={styles.getStartedContainer}>
           <Text style={styles.getStartedText}>Hello {this.props.name}!</Text>
-          <Text style={styles.getStartedText}>Too or Very?</Text>
-          <Text style={styles.lessonText}>
+
+          <Text style={styles.getStartedText}>
             What is the difference between “very” and “too”?
           </Text>
 
-          <Text>
-            {" "}
+          <Text style={styles.lessonText}>
             Students often confuse these two adverbs. Both come before
             adjectives or adverbs. Both make those words stronger. However,
             “very” does not have a negative meaning, while “too” does.
           </Text>
 
-          <Text>The test was “very” difficult.</Text>
-          <Text>The test was “too” difficult.</Text>
-          <Text>
+          <Text style={styles.lessonText}>The test was “very” difficult.</Text>
+          <Text style={styles.lessonText}>The test was “too” difficult.</Text>
+          <Text style={styles.lessonText}>
             If you say a test was “very” difficult, it challenged you, but you
             could still do it. In contrast, if you say a test was “too”
             difficult, you’re worried that you failed.
           </Text>
 
-          <Text>The bag was “very” expensive.</Text>
-          <Text>The bag was “too” expensive.</Text>
-          <Text>
+          <Text style={styles.lessonText}>The bag was “very” expensive.</Text>
+          <Text style={styles.lessonText}>The bag was “too” expensive.</Text>
+          <Text style={styles.lessonText}>
             Something that is “very” expensive can still be bought if you save
             up for it. However, an item that is “too” expensive must be left in
             the store.
           </Text>
 
-          <Text>
+          <Text style={styles.lessonText}>
             I hope this explanation was “very” helpful and not “too” confusing!
           </Text>
-          <Text>
-            Keep Swiping to work through the story. Then take the quiz to show
-            your understanding.
-          </Text>
+          <View>
+            <Text style={styles.getStartedText}> ---------- </Text>
+            <Text style={styles.getStartedText}>
+              Keep Swiping to work through the story. Then take the quiz to show
+              your understanding.
+            </Text>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
@@ -103,15 +105,13 @@ const styles = StyleSheet.create({
     height: 50
   },
   getStartedContainer: {
-    flex: 0.7,
-
-    marginHorizontal: 50
+    flex: 0.7
   },
   getStartedText: {
     fontSize: 20,
     color: "rgba(96,100,109, 1)",
     lineHeight: 24,
-    textAlign: "center",
+
     fontFamily: "monospace"
   },
 
